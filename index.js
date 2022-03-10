10271d2dbb890a0a9a0662989a5f839427aefcfd
Tomas <tbladykas@gmail.com>
2022-03-10 22:08:03 +0200

    uzd

---
+++ index.js
@@ -0,0 +1,14 @@
+//skaiciai
+
+let kint=15;
+let kint2=4;
+
+//let liekana=kint%kint2
+rez = kint % kint2
+console.log('Hello world')
+console.log(rez)
+
+let vardas='Tomas'
+let atsakymas = `${name}`
+let gim=1989;
+console.log(atsakymas)
