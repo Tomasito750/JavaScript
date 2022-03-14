91c5ecc7adb5727155c6f1accb6f50f5653bd03a
Tomas <tbladykas@gmail.com>
2022-03-14 22:03:07 +0200

    uzd

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ let vardas='Tomas'
 let atsakymas = `${name}`
 let gim=1989;
 console.log(atsakymas)
+
+
+let arbatpinigiai = prompt('Jusu moketina suma')
+let galutine = suma
+if (arbatpinigiai < 20)
+{
+    console.log('moketina suma:')
+    console.log(arbatpinigiai/10)}
+
+    if (20 <arbatpinigiai < 50)
+{
+    console.log('moketina suma:')
+    console.log((arbatpinigiai/100)*7.5)}
+
+    if (arbatpinigiai>50)
+{
+    console.log('moketinasuma:')
+    console.log(arbatpinigiai/20)}
+    
+    const baldai = ['lempa','lentyna', 'sofa','stalas'];
+    
